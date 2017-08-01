af66da6bb52205a4df73f49cab6247ade6323859
gitMonks <thanksimran@gmail.com>
2017-08-01 09:22:14 +0100

    adding the prjoect to git VCS

---
+++ client/main.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Router, Route, IndexRoute, browserHistory } from 'react-router';
+
+import { Bins } from '../imports/collections/bins';
+
+import App from './components/app';
+
+const routes = (
+		<Router history={ browserHistory} >
+			<Route path="/" component={App} >
+			</Route>
+		</Router>
+	);
+
+Meteor.startup(() => {
+	ReactDOM.render(<App />, document.querySelector('.render-target') );
+})
